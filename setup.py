cf6176de690461ab401db0b445657714d84d45bc
Rain Mark <rain.by.zhou@gmail.com>
2022-10-14 18:04:41 +0800

    update version
    Signed-off-by: Rain Mark <rain.by.zhou@gmail.com>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Oxfs is a user-space network file system similar to SSHFS, and the underlying da
 
 setup(
     name='oxfs',
-    version='0.5.3',
+    version='0.5.4',
     author='RainMark',
     author_email='rain.by.zhou@gmail.com',
     description='A Fast SFTP File System',
